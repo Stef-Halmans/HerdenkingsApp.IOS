ae1e4eaca20c4538ef9e063d61d7317225bdbc4b
Stef Halmans <j.p.halmans@iae.nl>
2019-11-17 14:12:03 -0800

    Naviagatie bar en hamburger menu werken nu op iedere pagina die een subclass is van baseviewcontroller, meerdere pagina's zijn toegevoegd, begonnen aan navigatie tussen pagina's

---
+++ HerdenkingsApp/HerdenkingsApp/Pages/MonumentenPage.swift
@@ -0,0 +1,26 @@
+//
+//  MonumentenPage.swift
+//  HerdenkingsApp
+//
+//  Created by Stef Halmans on 11/17/19.
+//  Copyright © 2019 Stef Halmans. All rights reserved.
+//
+
+import UIKit
+
+class MonumentenPage: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
--- HerdenkingsApp/HerdenkingsApp/ViewController.swift
+++ HerdenkingsApp/HerdenkingsApp/ViewController.swift
@@ -8,51 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: BaseViewController {
 
     
-    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
-    
-    @IBOutlet var masterPage: UIView!
-    
-    @IBOutlet weak var detailPage: UIView!
-    
-    var menuShowing = false
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let gesture = UITapGestureRecognizer(target: self, action:  #selector(self.HideDetailPage(_:)))
-        self.masterPage.addGestureRecognizer(gesture)
-        
-        detailPage.layer.shadowOpacity = 1
-        detailPage.layer.shadowRadius = 6
+
     }
-    @IBAction func HamburgerMenuClicked(_ sender: Any) {
+
+    
+    
+    
+    
+    
+
         
-        if(menuShowing){
-            leadingConstraint.constant = -255
-            UIView.animate(withDuration: 0.3, animations: {self.view.layoutIfNeeded()})
-        } else{
-           leadingConstraint.constant = 0
-            
-            UIView.animate(withDuration: 0.3, animations: {self.view.layoutIfNeeded()})
-        }
-        menuShowing = !menuShowing
-    }
     
-    
-    
-    
-    
-    @objc func HideDetailPage(_ sender:UITapGestureRecognizer){
-        if(menuShowing){
-            leadingConstraint.constant = -255
-            UIView.animate(withDuration: 0.3, animations: {self.view.layoutIfNeeded()})
-            menuShowing = false
-        }
-        
-    }
 
 }
 
