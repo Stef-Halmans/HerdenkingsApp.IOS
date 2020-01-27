2bd4cf92b87a4a0425573681c4634e48c510297b
Stef Halmans <stefhalmans@stefs-mac.local>
2020-01-27 12:01:23 -0800

    animaties gemaakt op de specifieke monumenten pagina om tussen verschillende monumenten te switchen

--- HerdenkingsApp/HerdenkingsApp/Pages/SpecifiekMonumentPage.swift
+++ HerdenkingsApp/HerdenkingsApp/Pages/SpecifiekMonumentPage.swift
@@ -10,40 +10,78 @@ import UIKit
 
 class SpecifiekMonumentPage: PageController {
 
-
+    @IBOutlet weak var monumentInformationView: UIView!
+    @IBOutlet weak var leadingMonument: NSLayoutConstraint!
+    @IBOutlet weak var trailingMonument: NSLayoutConstraint!
     
     @IBOutlet weak var monumentPicture: UIImageView!
     @IBOutlet weak var monumentName: UILabel!
     @IBOutlet weak var monumentDescription: UITextView!
+
+    
+    @IBOutlet weak var newMonumentInformationView: UIView!
+    @IBOutlet weak var leadingNewMonument: NSLayoutConstraint!
+    @IBOutlet weak var trailingNewMonument: NSLayoutConstraint!
+    
+    @IBOutlet weak var newMonumentPicture: UIImageView!
+    @IBOutlet weak var newMonumentName: UILabel!
+    @IBOutlet weak var newMonumentDescription: UITextView!
+    
+    
     @IBOutlet weak var gebiedName: UILabel!
+    
+    
+    private var firstView: Bool = true
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        monumentName.adjustsFontSizeToFitWidth = true
-
-        monumentenUitGebied = monumenten.sort(gebied: currentGebied)
+        initializePage()
         
-        gebiedName.text = Functions.enumToString(gebied: currentGebied)
-        monumentDescription.isScrollEnabled = true
-        monumentDescription.isUserInteractionEnabled = true
-        
-        initializeMonument()
+        initializeMonument(firstMonument: true)
 
     }
     
-//    override func viewDidAppear(_ animated: Bool) {
-//        monumentDescription.scrollRangeToVisible(NSMakeRange(0, 0))
-//    }
+    fileprivate func initializePage() {
 
-    func initializeMonument(){
         
-        monumentPicture.image = UIImage(named: monumentenUitGebied[monumentNummer].imageFilename)
-        monumentName.text = monumentenUitGebied[monumentNummer].title
-         monumentDescription.isScrollEnabled = false
-        monumentDescription.text = monumentenUitGebied[monumentNummer].description
+        leadingNewMonument.constant = self.view.bounds.width
+        trailingNewMonument.constant = self.view.bounds.width
+
+        monumentenUitGebied = monumenten.sort(gebied: currentGebied)
+
+        gebiedName.text = Functions.enumToString(gebied: currentGebied)
         monumentDescription.isScrollEnabled = true
-        monumentDescription.scrollRangeToVisible(NSMakeRange(0, 0))
+        monumentDescription.isUserInteractionEnabled = true
+}
+    
+
+    func initializeMonument(firstMonument: Bool){
+        if(firstMonument){
+            monumentPicture.image = UIImage(named:
+                monumentenUitGebied[monumentNummer].imageFilename)
+            monumentName.text = monumentenUitGebied[monumentNummer].title
+            monumentDescription.isScrollEnabled = false
+            monumentDescription.text = monumentenUitGebied[monumentNummer].description
+            monumentDescription.isScrollEnabled = true
+            monumentDescription.scrollRangeToVisible(NSMakeRange(0, 0))
+                    monumentName.adjustsFontSizeToFitWidth = true
+
+        }
+        else{
+            newMonumentPicture.image = UIImage(named:
+                monumentenUitGebied[monumentNummer].imageFilename)
+            newMonumentName.text = monumentenUitGebied[monumentNummer].title
+            newMonumentDescription.isScrollEnabled = false
+            newMonumentDescription.text = monumentenUitGebied[monumentNummer].description
+            newMonumentDescription.isScrollEnabled = true
+            newMonumentDescription.scrollRangeToVisible(NSMakeRange(0, 0))
+                    newMonumentName.adjustsFontSizeToFitWidth = true
+        }
+
+
+        
         
         
     }
@@ -55,7 +93,12 @@ class SpecifiekMonumentPage: PageController {
         else if(monumentNummer < monumentenUitGebied.count - 1){
             monumentNummer += 1
         }
-        initializeMonument()
+        leadingNewMonument.constant = self.view.bounds.width
+        trailingNewMonument.constant = self.view.bounds.width
+        self.view.superview?.layoutIfNeeded()
+        initializeMonument(firstMonument: false)
+        changeMonumentAnimation(next: true)
+        
     }
 
     
@@ -66,7 +109,34 @@ class SpecifiekMonumentPage: PageController {
         else if (monumentNummer == 0){
             monumentNummer = monumentenUitGebied.count - 1
         }
-        initializeMonument()
+        leadingNewMonument.constant = -self.view.bounds.width
+        trailingNewMonument.constant = -self.view.bounds.width
+        self.view.superview?.layoutIfNeeded()
+        initializeMonument(firstMonument: false)
+        changeMonumentAnimation(next: false)
+    }
+    
+    func changeMonumentAnimation(next: Bool){
+        
+        self.leadingNewMonument.constant = 0
+        self.trailingNewMonument.constant = 0
+        self.leadingMonument.constant = (next) ? -self.view.bounds.width : self.view.bounds.width
+        self.trailingMonument.constant =  (next) ? self.view.bounds.width : -self.view.bounds.width
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.superview?.layoutIfNeeded()
+            self.monumentInformationView.alpha = 1
+            
+            
+            
+            
+        }){ _ in
+            self.initializeMonument(firstMonument: true)
+            self.leadingMonument.constant = 0
+            self.trailingMonument.constant = 0
+            self.leadingNewMonument.constant = self.view.bounds.width
+            self.trailingNewMonument.constant = self.view.bounds.width
+        }
     }
 
     
